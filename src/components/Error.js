98c7e0930fe84e3f32b24a956cbe983b2559ea21
ANS <36783010+answebdev@users.noreply.github.com>
2022-10-16 10:29:48 -0700

    add error page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 import { Helmet } from 'react-helmet';
-import logo from '../logo.svg';
+import error from '../error.jpg';
 import './Error.css';
 
 const Error = () => {
@@ -12,11 +12,12 @@ const Error = () => {
         </Helmet>
       </div>
       <header className='Error-header'>
-        <img src={logo} className='Error-logo' alt='logo' />
         <p className='Error-message'>Sorry, page not found.</p>
         <Link className='main-link' to='/'>
           Home
         </Link>
+        <br />
+        <img src={error} className='Error-logo' alt='logo' />
       </header>
     </div>
   );
