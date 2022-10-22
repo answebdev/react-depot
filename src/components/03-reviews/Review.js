77c4ac49b71a55039606dd161525fbcf2c9d24f8
ANS <36783010+answebdev@users.noreply.github.com>
2022-10-22 10:24:59 -0700

    add buttons

--- src/components/03-reviews/Review.js
+++ src/components/03-reviews/Review.js
@@ -12,7 +12,22 @@ const Review = () => {
     <article className='review'>
       <div className='img-container'>
         <img className='person-img' src={image} alt={name} />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
       </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn'>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn'>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>surprise me</button>
     </article>
   );
 };
