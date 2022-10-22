d1b69091dfbf5efc4cf3b7ffd64ef993d611e75b
ANS <36783010+answebdev@users.noreply.github.com>
2022-10-22 11:27:02 -0700

    add logic for next and previous button

--- src/components/03-reviews/Review.js
+++ src/components/03-reviews/Review.js
@@ -6,7 +6,41 @@ const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
-  // console.log(people)
+  // console.log(people);
+
+  // After clicking the next/previous buttons, eventually, you will get to the end of the list of people.
+  // In this case, there are only 4 people in the data array, and when you reach the end of the array and try to click next/previous,
+  // you will get an error because there is no other person next in the array - there is no other index.
+
+  // To solve this, we do this:
+  // If the number is bigger than the last number of the array, then return 0.
+  // If the number is less than 0, then return the last item in the array.
+  const checkNumber = (number) => {
+    // Last number in the array:
+    if (number > people.length - 1) {
+      return 0;
+    }
+    // If the number is less than 0, return the last item of the array
+    if (number < 0) {
+      return people.length - 1;
+    }
+    // If the number is neither of these two cases, then simply return the number
+    return number;
+  };
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
 
   return (
     <article className='review'>
@@ -20,10 +54,10 @@ const Review = () => {
       <p className='job'>{job}</p>
       <p className='info'>{text}</p>
       <div className='button-container'>
-        <button className='prev-btn'>
+        <button className='prev-btn' onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className='next-btn'>
+        <button className='next-btn' onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
