108b3f15a46181f2dad3718ea39f9c44c218bb57
ANS <36783010+answebdev@users.noreply.github.com>
2023-02-19 11:45:01 -0800

    update sitemap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { hydrate, render } from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -12,5 +13,12 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+const rootElement = document.getElementById('root');
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
 
 reportWebVitals();
