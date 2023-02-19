32bf2718c3b858378fb78b24c39026328872ba8b
ANS <36783010+answebdev@users.noreply.github.com>
2023-02-19 10:02:06 -0800

    add sitemap

--- cypress/e2e/02_second_suite.cy.js
+++ cypress/e2e/02_second_suite.cy.js
@@ -10,4 +10,114 @@ describe('Second test suite', () => {
     cy.log('Checking that h2 tag has "Our Tours" text');
     cy.get('[data-testid=header]').should('have.text', 'Our Tours');
   });
+
+  it('removes first tour card', () => {
+    cy.log(
+      'checks that "Best of Paris in 7 Days Tour" text displays on first card'
+    );
+    cy.get('[data-testid=tour-name]').should(
+      'contain.text',
+      'Best of Paris in 7 Days Tour'
+    );
+
+    cy.get('[data-testid=tour-name]')
+      .eq(0)
+      .should('contain', 'Best of Paris in 7 Days Tour');
+
+    cy.log('checks that Not Interested button removes the first tour card');
+    cy.get('[data-testid=delete-btn]')
+      .eq(0)
+      .click();
+  });
+
+  it('shows more text on second tour card', () => {
+    cy.log(
+      'checks that "Best of Paris in 7 Days Tour" text displays on first card'
+    );
+    cy.get('[data-testid=tour-name]').should(
+      'contain.text',
+      'Best of Paris in 7 Days Tour'
+    );
+
+    cy.get('[data-testid=tour-name]')
+      .eq(0)
+      .should('contain', 'Best of Paris in 7 Days Tour');
+
+    cy.log('checks that Not Interested button removes the first tour card');
+    cy.get('[data-testid=delete-btn]')
+      .eq(0)
+      .click();
+
+    // ============================
+
+    cy.log(
+      'checks that "Best of Ireland in 14 Days Tour" text displays on second card'
+    );
+
+    cy.get('[data-testid=tour-name]').should(
+      'contain.text',
+      'Best of Ireland in 14 Days Tour'
+    );
+
+    cy.get('[data-testid=tour-name]')
+      .eq(0)
+      .should('contain', 'Best of Ireland in 14 Days Tour');
+
+    cy.log('checks that Read More button shows more text on second card');
+    cy.get('[data-testid=read-more-less]')
+      .eq(0)
+      .click();
+  });
+
+  // ========================
+
+  it('shows less text on second tour card', () => {
+    cy.log(
+      'checks that "Best of Paris in 7 Days Tour" text displays on first card'
+    );
+    cy.get('[data-testid=tour-name]').should(
+      'contain.text',
+      'Best of Paris in 7 Days Tour'
+    );
+
+    cy.get('[data-testid=tour-name]')
+      .eq(0)
+      .should('contain', 'Best of Paris in 7 Days Tour');
+
+    cy.log('checks that Not Interested button removes the first tour card');
+    cy.get('[data-testid=delete-btn]')
+      .eq(0)
+      .click();
+
+    // ============================
+
+    // NEED TO CHECK THIS TEST - I THINK IT CURRENTLY CLICKS ON 'SEE LESS' BUTTON OF ALL CARDS
+    // INSTEAD OF JUST THE FIRST ONE.
+
+    // ALSO: MAYBE REMOVE 'BEFORE EACH' UP TOP AND RE-WRITE TESTS.
+    // WRITE NOW, IT'S VISTING '/tours' BEFORE EACH TEST.
+
+    cy.log(
+      'checks that "Best of Ireland in 14 Days Tour" text displays on second card'
+    );
+
+    cy.get('[data-testid=tour-name]').should(
+      'contain.text',
+      'Best of Ireland in 14 Days Tour'
+    );
+
+    cy.get('[data-testid=tour-name]')
+      .eq(0)
+      .should('contain', 'Best of Ireland in 14 Days Tour');
+
+    cy.log('checks that Show Less button shows less text on second card');
+    // cy.get('[data-testid=read-more-less]');
+
+    if (cy.get('[data-testid=read-more-less]').length > 0) {
+      // Evaluates as true
+      cy.get('[data-testid=read-more-less]')
+        .eq(0)
+        .click();
+    }
+  });
 });
--- src/components/02-tours/Tour.js
+++ src/components/02-tours/Tour.js
@@ -8,17 +8,24 @@ const Tour = ({ id, image, info, price, name, removeTour }) => {
       <img src={image} alt={name} />
       <footer>
         <div className='tour-info'>
-          <h4>{name}</h4>
+          <h4 data-testid='tour-name'>{name}</h4>
           <h4 className='tour-price'>${price}</h4>
         </div>
         {/* 'substring(0,200)' means display 200 characters of 'info' text (includes spaces): */}
         <p>
           {readMore ? info : `${info.substring(0, 200)}...`}
-          <button onClick={() => setReadMore(!readMore)}>
+          <button
+            data-testid='read-more-less'
+            onClick={() => setReadMore(!readMore)}
+          >
             {readMore ? 'show less' : 'read more'}
           </button>
         </p>
-        <button className='delete-btn' onClick={() => removeTour(id)}>
+        <button
+          data-testid='delete-btn'
+          className='delete-btn'
+          onClick={() => removeTour(id)}
+        >
           Not Interested
         </button>
       </footer>
